a16f4a79332330e947d8ced769afe5ba98fcc8b0
stardust-sudo <2546535401@qq.com>
2022-06-26 10:39:27 +0800

    first commit

---
+++ src/main/java/com/stardust/demo/GitTest.java
@@ -0,0 +1,11 @@
+package com.stardust.demo;
+
+/**
+ * @author stardust
+ * @create 2022-06-26 10:35
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git");
+    }
+}
